820fcf734686c7ff76d9ca093d9badd088834cdd
Justin Kaseman <justinkaseman@live.com>
2018-11-12 15:49:21 -0800

    expand quicksort

--- Algorithms/Sorting/Quick.js
+++ Algorithms/Sorting/Quick.js
@@ -4,9 +4,10 @@
         3) Call quicksort on sub-arrays
 */
 
+// Basic Implementation : Pivot is first element
 const quicksort = array => {
   if (array.length < 1) return array;
-  const pivot = array[0];
+  const pivot = array[array.length / 2];
   const lower = [];
   const higher = [];
   for (let i = 1; i < array.length; i++) {
@@ -19,3 +20,11 @@ const quicksort = array => {
 };
 
 console.log(quicksort([1, 5, 3, 2, 10, 9]));
+
+/* 
+Average runtime of O(n log n), worst runtime of O(n^2) 
+Performance depends a lot of the pivot that is chosen
+The constant in quicksort makes it faster than mergesort
+*/
+
+// Hoare and Lomuto partition sceheme
